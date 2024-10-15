548cdd20c0cf651f4d5902944a2367782637c330
Joel Neander <jn223wj@student.lnu.se>
2024-10-15 14:39:21 +0200

    man kan nu ändra storleken på bilden man har valt

--- app/src/main/java/org/example/app/Frame.java
+++ app/src/main/java/org/example/app/Frame.java
@@ -9,12 +9,20 @@ import javax.swing.filechooser.*;
 import java.io.File;
 import app.ImageEditor;
 import javax.imageio.ImageIO;
+import app.ImageResizer;
+import java.awt.image.BufferedImage;
 
 public class Frame extends JFrame implements ActionListener{
 
-    JButton uploadBtn;
-    JButton saveBtn;
-    ImageEditor editor;
+    private JButton uploadBtn;
+    private JButton saveBtn;
+    private JButton resizeBtn;
+    private ImageEditor editor;
+    private JPanel mainPanel = new JPanel();
+    private JPanel editButtonsPanel = new JPanel();
+    private JTextField height;
+    private JTextField width;
+
 
     Frame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -32,12 +40,34 @@ public class Frame extends JFrame implements ActionListener{
         saveBtn.addActionListener(this);
         saveBtn.setSize(40, 40);
 
-        JPanel panel = new JPanel();
-        panel.add(uploadBtn);
-        panel.add(saveBtn);
-        panel.setBackground(new Color(0,0,0));
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(uploadBtn);
+        buttonPanel.add(saveBtn);
 
-        this.setContentPane(panel);
+        resizeBtn = new JButton("Resize Image");
+        resizeBtn.setFocusable(false);
+        resizeBtn.addActionListener(this);
+        resizeBtn.setSize(40, 40);
+
+        JLabel label1 = new JLabel("Height:");
+        height = new JTextField(8);
+        height.setBounds(0, 0, 150, 10);
+
+        JLabel label2 = new JLabel("Width:");
+        width = new JTextField(8);
+        width.setBounds(0, 0, 150, 100);
+
+        editButtonsPanel.add(resizeBtn);
+        editButtonsPanel.add(label1);
+        editButtonsPanel.add(height);
+        editButtonsPanel.add(label2);
+        editButtonsPanel.add(width);
+        
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+        mainPanel.add(buttonPanel, BorderLayout.PAGE_END);
+        mainPanel.setBackground(new Color(0,0,0));
+
+        this.setContentPane(mainPanel);
         this.setVisible(true);
     }
 
@@ -57,6 +87,7 @@ public class Frame extends JFrame implements ActionListener{
                     //ImageEditor editor = new ImageEditor(file_path.getAbsolutePath());
                     editor.setImage(ImageIO.read(new File(file_path.getAbsolutePath())));
                     //editor.saveImage("png", file_path.getParent() + "/shalle1.png");
+                    showImage(file_path.getAbsolutePath());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -75,6 +106,38 @@ public class Frame extends JFrame implements ActionListener{
                     e.printStackTrace();
                 }
             }
+        } else if (event.getSource() == resizeBtn) {
+            int newWidth;
+            int newHeight;
+            try {
+                newWidth = Integer.parseInt(width.getText());
+                newHeight = Integer.parseInt(height.getText());
+            } catch (NumberFormatException e) {
+                break;
+            }
+            ImageResizer resizer = new ImageResizer();
+            BufferedImage resizedImage = resizer.resizeImage(editor.getImage(), newWidth, newHeight);
+            editor.setImage(resizedImage);
         }
+    }
+
+    void showImage(String imagePath) {
+        JPanel imagePanel = new JPanel() {
+            @Override
+            protected void paintComponent (Graphics g) {
+                super.paintComponent(g);
+                ImageIcon icon = new ImageIcon(imagePath);
+                Image image = icon.getImage();
+
+                if (image != null) {
+                    int x = (getWidth() - image.getWidth(null)) / 2;
+                    int y = (getHeight() - image.getHeight(null)) / 2;
+                    g.drawImage(image, x, y, this);
+                }
+            }
+        };
+        mainPanel.add(imagePanel, BorderLayout.CENTER);
+        mainPanel.add(editButtonsPanel, BorderLayout.CENTER);
+        this.setVisible(true);
     }
 }
